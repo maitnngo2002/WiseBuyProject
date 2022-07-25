57b99d9ef7414443c7f82db214da31f0ba011a84
maingo <ngothingocmaiclht@gmail.com>
2022-07-25 12:22:46 -0700

    remove commented out code

--- WiseBuy/Views/DealCell.m
+++ WiseBuy/Views/DealCell.m
@@ -8,21 +8,13 @@
 #import "DealCell.h"
 #import "Parse/Parse.h"
 #import "Item.h"
+#import "DatabaseManager.h"
 
 @implementation DealCell
 
 - (void)setDeal:(AppDeal *)deal {
     _deal = deal;
-    NSData *itemImage = _deal.item.image;
-//    NSLog(@"%@", [itemImage ]);
-//    NSURL *url = [NSURL URLWithString:itemImage.url];
-//    NSData *data = [NSData dataWithContentsOfURL:url];
-//    self.itemImageView.image = [UIImage imageWithData:_deal.item.image];
-
-//    UIImage *img = [[UIImage alloc] initWithData:itemImage];
     self.itemImage.image = [UIImage imageWithData:_deal.item.image];
-
-//    self.itemImage.image = image;
     self.itemName.text = _deal.item.name;
     self.sellerName.text = _deal.sellerName;
     self.price.text = [DealCell formattedPrice:_deal.price];
